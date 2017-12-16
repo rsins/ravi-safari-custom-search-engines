58e3698a08019cb91066a36c613fd4998e503d15
Singh, Ravi <raviksingh@paypal.com>
2017-12-16 10:08:19 +0530

    split word search, multiple search tags in URL permitted now.

--- Custom-Search-Engine.safariextension/background-safari.js
+++ Custom-Search-Engine.safariextension/background-safari.js
@@ -6,7 +6,7 @@ safari.extension.settings.addEventListener("change", function(e) {
     	var safariWindow = safari.application.activeBrowserWindow;
     	var tab = safariWindow.openTab("foreground");
     	if (!safariWindow.visible) safariWindow.activate();
-    	tab.url = safari.extension.baseURI + "safari_options.html";
+    	tab.url = safari.extension.baseURI + "options.html";
     }
 }, false);
 
